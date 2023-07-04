a0902ac5ef82cbbd952fc4980ad52bba4aca7b1a
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-05 03:40:09 +0700

    Update script.php

--- script.php
+++ script.php
@@ -3,24 +3,30 @@ $isi = file_get_contents("https://raw.githubusercontent.com/mahdibland/V2RayAggr
 $baris = explode("\n", $isi);
 // Menghitung total baris
 $totalBaris = count($baris);
-
+$urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+file_put_contents("a.yaml", "proxies:");
 // Menginisialisasi variabel untuk menyimpan hasil URL
-$i = 0;
+$in = 0;
 while (true) {
-  $urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+  //$urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+  for($i=0; $i < $totalBaris; $i++) {
   $bagian = $baris[$i];
   echo $i." => ".$bagian."\n";
-  $bagianTeks = $bagian;
+  $bagianTeks = $bagian."|";
   $bagianUrl = urlencode($bagianTeks);
   $urlHasil .= $bagianUrl;
+  if($i == $in + 49 or $i == $totalBaris){
   $urlHasil .="&insert=false&config=base%2Fdatabase%2Fconfig%2Fstandard%2Fstandard_redir.ini&filename=a.yaml&emoji=true&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true";
   $url = file_get_contents($urlHasil);
   $hasil = explode("proxies:", $url)[1];
   $hasil = explode("proxy-groups:", $hasil)[0];
   file_put_contents("a.yaml", $hasil, FILE_APPEND);
-  $i++;
+  $in +=49;
+  $urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+  }
   if ($i == $totalBaris) {
     break;
+  }
   }
 }
 ?>
