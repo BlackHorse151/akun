151a062afd33e02673d94ecaca6569bd2201bea5
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-12 22:41:00 +0700

    Update convert.php

--- convert.php
+++ convert.php
@@ -5,36 +5,50 @@ use Symfony\Component\Yaml\Yaml;
 
 function convertToV2RayLink($yaml) {
     $proxies = Yaml::parse($yaml)['proxies'];
+    $vmessUrls = [];
+
     foreach ($proxies as $proxy) {
         $vmessUrl = 'vmess://';
 
-        $name = $proxy['servername'];
         $server = $proxy['server'];
         $port = $proxy['port'];
         $uuid = $proxy['uuid'];
         $alterId = $proxy['alterId'];
         $cipher = $proxy['cipher'];
         $tls = $proxy['tls'];
+        if($tls == 1) {
+            $tls = "tls";
+        }
         $skipCertVerify = $proxy['skip-cert-verify'];
         $servername = $proxy['servername'];
         $network = $proxy['network'];
-        $path = urlencode($proxy['ws-opts']['path']);
+        $path = $proxy['ws-opts']['path'];
         $headersHost = urlencode($proxy['ws-opts']['headers']['Host']);
         $udp = $proxy['udp'];
 
-        $vmessUrl .= base64_encode("{$server}:{$port}/?add={$servername}&aid={$alterId}&host={$servername}&path=%2F{$path}&tls={$tls}&sni={$servername}&type=ws&v=2&security={$cipher}&servername={$servername}&network={$network}&wsHeaders=Host%3A%20{$headersHost}&udp={$udp}&allowInsecure={$skipCertVerify}");
-
-        $vmessUrls[] = [
-            'name' => $name,
-            'url' => $vmessUrl
-        ];
+        $vmessUrl .= base64_encode(json_encode([
+            'add' => $server,
+            'aid' => $alterId,
+            'host' => $servername,
+            'id' => $uuid,
+            'path' => "{$path}",
+            'tls' => $tls,
+            'sni' => $servername,
+            'port' => $port,
+            'v' => '2',
+            'scy' => $cipher,
+            'ps' => $servername,
+            'net' => $network,
+            'allowInsecure' => $skipCertVerify,
+        ]));
+        $vmessUrls[] = $vmessUrl;
     }
     return $vmessUrls;
 }
 $v2rayLinks = convertToV2RayLink(file_get_contents("b.yaml"));
 file_put_contents("sing-box.txt", "");
 foreach ($v2rayLinks as $link) {
-    echo $link["url"] . "\n";
-    file_put_contents("sing-box.txt", $link["url"]."\n", FILE_APPEND);
+    echo $link . "\n";
+    file_put_contents("sing-box.txt", $link."\n", FILE_APPEND);
 }
 ?>
