6c08e665b0a818507d11506075a79a02d80d7f75
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-13 06:52:23 +0700

    Update speedtest.sh

--- utils/speedtest.sh
+++ utils/speedtest.sh
@@ -1,5 +1,2 @@
-#准备好所需文件
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64-v3-v0.15.0.gz
-gzip -d lite-linux-amd64.gz
 chmod +x ./lite-linux-amd64
-./lite-linux-amd64 --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt>speedtest.log
+./lite-linux-amd64 --config config.json --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt>speedtest.log
