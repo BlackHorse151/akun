da898f1fc825c9577ebd033d6e6a36540892fa3c
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-13 02:19:17 +0700

    Update speedtest.sh

--- utils/speedtest.sh
+++ utils/speedtest.sh
@@ -1,5 +1,5 @@
 #准备好所需文件
-wget -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
+wget -O lite-linux-amd64 https://github.com/Dreamacro/clash/releases/download/v1.11.8/clash-linux-amd64-v1.11.8.gz
 wget -O lite_config.json https://raw.githubusercontent.com/ardi5209/akun/main/utils/lite_config.json
 #运行 LiteSpeedTest
 chmod +x ./lite-linux-amd64
