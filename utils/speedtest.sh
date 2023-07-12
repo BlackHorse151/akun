e97758cf622178ed5fd2eb6d67db0ee5b8e77e4e
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-13 06:57:04 +0700

    Update speedtest.sh

--- utils/speedtest.sh
+++ utils/speedtest.sh
@@ -1,2 +1,2 @@
 chmod +x ./lite-linux-amd64
-./lite-linux-amd64 --config config.json --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt>speedtest.log
+./lite-linux-amd64 --config config.json
