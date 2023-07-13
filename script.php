a3f6c87516d83775bd924672edbc40c5beb2fd92
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-13 12:11:23 +0700

    Update script.php

--- script.php
+++ script.php
@@ -2,28 +2,39 @@
 //https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge.txt
 //https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/All_Configs_Sub.txt
 //https://raw.githubusercontent.com/yebekhe/ConfigCollector/main/sub/mix_base64
-//$url = array("https://raw.githubusercontent.com/ardi5209/V2RayAggregator/master/sub/sub_merge.txt","https://raw.githubusercontent.com/ardi5209/ConfigCollector/main/sub/mix");
-
+$url = [
+    "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge_base64.txt",
+    "https://raw.githubusercontent.com/yebekhe/ConfigCollector/main/sub/mix_base64",
+    "https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt",
+    "https://raw.githubusercontent.com/Bardiafa/Free-V2ray-Config/main/All_Configs_Sub.txt",
+];
+//https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt
+$uc = count($url) - 1;
 file_put_contents("a.yaml", "proxies:");
-//foreach($url as $link) {
-$isi = base64_decode(file_get_contents("https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt"));
-$baris = explode("\n", $isi);
-// Menghitung total baris
-$totalBaris = count($baris);
-$urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
-//file_put_contents("a.yaml", "proxies:");
-// Menginisialisasi variabel untuk menyimpan hasil URL
-$in = 0;
-    //$urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+foreach ($url as $link) {
+    if ($url[$uc] == $link) {
+        $isi = file_get_contents($link);
+    } else {
+        $isi = base64_decode(file_get_contents($link));
+    }
+    $baris = explode("\n", $isi);
+    // Menghitung total baris
+    $totalBaris = count($baris);
+    $urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+    //file_put_contents("a.yaml", "proxies:");
+    // Menginisialisasi variabel untuk menyimpan hasil URL
+    $in = 0;
+    $urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
     for ($i = 0; $i < $totalBaris; $i++) {
         $bagian = $baris[$i];
-        //if(preg_match("/(vmess|vless|trojan)/i", $bagian)) {
+        if (preg_match("/(vmess)/i", $bagian)) {
             echo $i . " => " . $bagian . "\n";
             $bagianTeks = $bagian . "|";
             $bagianUrl = urlencode($bagianTeks);
             $urlHasil .= $bagianUrl;
             if ($i >= $in + 40 or $i >= $totalBaris) {
-                $urlHasil .="&insert=false&config=base%2Fdatabase%2Fconfig%2Fstandard%2Fstandard_redir.ini&filename=a.yaml&emoji=true&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true";
+                $urlHasil .=
+                    "&insert=false&config=base%2Fdatabase%2Fconfig%2Fstandard%2Fstandard_redir.ini&filename=a.yaml&emoji=true&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true";
                 $url = file_get_contents($urlHasil);
                 $hasil = explode("proxies:", $url)[1];
                 $hasil = explode("proxy-groups:", $hasil)[0];
@@ -31,10 +42,10 @@ $in = 0;
                 $in += 40;
                 $urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
             }
-        //}
+        }
         if ($i == $totalBaris or $i >= $totalBaris) {
             break;
         }
     }
-//}
+}
 ?>
