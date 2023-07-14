ea6f9d5754aa068bb637f9b7c2588a713c9ace00
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-14 08:34:50 +0700

    Update script.php

--- script.php
+++ script.php
@@ -4,6 +4,7 @@
 //https://raw.githubusercontent.com/yebekhe/ConfigCollector/main/sub/mix_base64
 $url = [
     "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/vmess_base64",
+    "https://raw.githubusercontent.com/adiwzx/freenode/main/adispeed.txt",
     "https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt",
 ];
 //https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt
