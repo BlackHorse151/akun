2420557c8e1ee87075652a24499e0629d0c7a7d6
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-18 11:01:11 +0700

    Update new.php

--- new.php
+++ new.php
@@ -109,7 +109,7 @@ foreach($akun as $i) {
     if (preg_match("/(vless|vmess|trojan):\/\//i", $i)) {
         echo $i."\n";
         $url = "https://sub.bonds.id/sub2?target=clash&url=";
-        $url .= $i;
+        $url .= urlencode($i);
         $url .= "&insert=false&config=base%2Fdatabase%2Fconfig%2Fstandard%2Fstandard_redir.ini&filename=a.yaml&emoji=true&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true";
         $hasil = file_get_contents($url);
         $hasil = explode("proxies:", $hasil)[1];
