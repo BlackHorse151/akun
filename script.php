6f634030c22fe9d0afa162b589d60247454dd349
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-29 09:02:08 +0700

    Update script.php

--- script.php
+++ script.php
@@ -14,10 +14,11 @@ $url = [
     "https://raw.githubusercontent.com/LonUp/NodeList/main/V2RAY/003.txt",
     "https://raw.githubusercontent.com/mianfeifq/share/main/README.md",
     "https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list_raw.txt",
-    //"https://muma16fx.netlify.app/",
-    //"https://youlianboshi.netlify.app/",
-    //"https://qiaomenzhuanfx.netlify.app/",
-    //"https://raw.githubusercontent.com/HakurouKen/free-node/main/public",
+    "https://muma16fx.netlify.app/",
+    "https://youlianboshi.netlify.app/",
+    "https://qiaomenzhuanfx.netlify.app/",
+    "https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray",
+    "https://raw.githubusercontent.com/HakurouKen/free-node/main/public",
 ];
 //https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt
 file_put_contents("a.yaml", "proxies:");
