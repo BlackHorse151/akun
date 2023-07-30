ae9eab3757495347b02038cc921647dea03e793e
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-07-30 11:46:00 +0700

    Update script.php

--- script.php
+++ script.php
@@ -24,6 +24,7 @@ $url = [
     "https://qiaomenzhuanfx.netlify.app/",
     "https://clashnode.com/wp-content/uploads/2023/07/20230708.txt",
     "https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray",
+    "https://raw.githubusercontent.com/tbbatbb/Proxy/master/dist/v2ray.config.txt",
     "https://raw.githubusercontent.com/HakurouKen/free-node/main/public",
 ];
 //https://raw.githubusercontent.com/snakem982/proxypool/main/v2ray.txt
