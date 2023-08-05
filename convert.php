c8e12af23a41ad44d42db7d2d388f1c2875d8aea
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-08-06 05:58:43 +0700

    Update convert.php

--- convert.php
+++ convert.php
@@ -81,8 +81,7 @@ foreach ($formats as $format) {
 }
 file_put_contents("sing-box-base64.txt", base64_encode($hasil));
 //shell_exec("gzip -d lite-linux-amd64.gz");
-shell_exec("chmod +x ./lite-linux-amd64");
-shell_exec("./lite-linux-amd64 --config config.json --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt");
+shell_exec("chmod +x ./lite-linux-amd64 && ./lite-linux-amd64 --config config.json --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt");
 $speedtest = json_decode(file_get_contents("output.json"));
 file_put_contents("sing-box.txt", "");
 file_put_contents("hasil_convert(untest).yaml", "proxies:");
