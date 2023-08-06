802709ee32468dc106f076c76000eb1dedaf0d18
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-08-06 10:58:33 +0700

    Update convert.php

--- convert.php
+++ convert.php
@@ -84,11 +84,18 @@ file_put_contents("sing-box-base64.txt", base64_encode($hasil));
 shell_exec("chmod +x ./lite-linux-amd64 && ./lite-linux-amd64 --config config.json --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt");
 $speedtest = json_decode(file_get_contents("output.json"));
 file_put_contents("sing-box.txt", "");
+file_put_contents("sing-box.yaml", "");
 file_put_contents("hasil_convert(untest).yaml", "proxies:");
 $query = "&insert=false&config=base%2Fdatabase%2Fconfig%2Fstandard%2Fstandard_redir.ini&filename=a.yaml&emoji=true&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true";
 foreach ($speedtest->nodes as $akun) {
 	if ($akun->isok == true) {
 		file_put_contents("sing-box.txt", $akun->link . "\n", FILE_APPEND);
+		$urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
+		$akn = urlencode($akun->link);
+		$url = file_get_contents($urlHasil . $akn . $query);
+		$hasil = explode("proxies:", $url) [1];
+		$hasil = explode("proxy-groups:", $hasil) [0];
+		file_put_contents("sing-box.yaml", $hasil, FILE_APPEND);	
 	}
 	$urlHasil = "https://sub.bonds.id/sub2?target=clash&url=";
 	$akn = urlencode($akun->link);
