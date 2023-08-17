af67f269a66b5b2649713be927889d55e9a5fb61
ardi5209 <79729284+ardi5209@users.noreply.github.com>
2023-08-17 07:53:29 +0700

    Update convert.php

--- convert.php
+++ convert.php
@@ -84,7 +84,7 @@ file_put_contents("sing-box-base64.txt", base64_encode($hasil));
 shell_exec("chmod +x ./lite-linux-amd64 && ./lite-linux-amd64 --config config.json --test https://raw.githubusercontent.com/ardi5209/akun/main/sing-box-base64.txt");
 $speedtest = json_decode(file_get_contents("output.json"));
 file_put_contents("sing-box.txt", "");
-file_put_contents("sing-box.yaml", "");
+file_put_contents("sing-box.yaml", "proxies:");
 file_put_contents("hasil_convert(untest).yaml", "proxies:");
 $query = "&insert=false&config=base%2Fdatabase%2Fconfig%2Fstandard%2Fstandard_redir.ini&filename=a.yaml&emoji=true&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true";
 foreach ($speedtest->nodes as $akun) {
